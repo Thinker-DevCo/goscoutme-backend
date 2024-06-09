a53b056f2cdd9dffc648ea2ddcf4d0b75f377991
Thinker-Dev <107056866+Thinker-Dev@users.noreply.github.com>
2024-06-09 03:19:27 +0200

    time presigned

--- src/useCases/Media/Media.service.ts
+++ src/useCases/Media/Media.service.ts
@@ -39,7 +39,7 @@ class MediaUseCase {
       Bucket: 'goscoutme',
       Key: `${user_id}/${file_name}`,
       ACL: 'public-read-write'
-    }))
+    }), {expiresIn: 600})
     return signedUrl
   }
 
