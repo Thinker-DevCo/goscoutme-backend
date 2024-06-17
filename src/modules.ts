eec3af2fc616eafcda1804d1ba30da447cdf70c6
Thinker-Dev <107056866+Thinker-Dev@users.noreply.github.com>
2024-06-17 08:24:42 +0200

    created notifciations routes

--- src/modules.ts
+++ src/modules.ts
@@ -7,8 +7,9 @@ import { SportsRoutes } from "./useCases/Sports/sports.route";
 import { MediaRoutes } from "./useCases/Media/Media.route";
 import { AppointmentsRoutes } from "./useCases/Appointments/appointments.route";
 import { NotificationsController } from "./useCases/Notifications/notifications.controller";
+import { NotificationsRoutes } from "./useCases/Notifications/notifications.route";
 
-@GlobalRoutes([AuthRoutes, ProfileRoutes, SportsRoutes, MediaRoutes, AppointmentsRoutes, NotificationsController])
+@GlobalRoutes([AuthRoutes, ProfileRoutes, SportsRoutes, MediaRoutes, AppointmentsRoutes, NotificationsRoutes])
 export class AppGlobalRoutes {
   startModule(app: Express): void {}
 }
